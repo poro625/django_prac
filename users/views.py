a06617fd866bc9ecf1c61d09fda362a70162e6b7
poro625 <poro625@naver.com>
2022-10-24 21:16:48 +0900

    회원가입,로그인 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import email
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from .models import User
+from django.contrib.auth import authenticate, login as loginsession
+
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    elif request.method == 'POST':
+        email =  request.POST.get('email') #이메일
+        username =  request.POST.get('username') # 유저네임
+        nickname =  request.POST.get('nickname') # 닉네임
+        password =  request.POST.get('password') # 비밀번호
+        password2 =  request.POST.get('password2') # 비밀번호 확인
+
+        if password == password2:
+            User.objects.create_user(email=email, username = username, nickname=nickname, password = password)
+
+            return HttpResponse('회원가입 완료')
+        
+        else :
+
+            return render(request, 'signup.html')
+
+    else :
+
+        return HttpResponse('허용되지 않은 메소드 입니다')
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password =  request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user:
+            loginsession(request, user)
+        context = {
+
+        'email' : email,
+        'password' : password,
+
+        }            
+        return render(request ,'home.html' , context)
+    else:
+        return HttpResponse('로그인 실패')
+
+def home(request):
+    return render(request, 'home.html')
