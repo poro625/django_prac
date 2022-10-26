42c9e6ba792dec4fd522c14bcb4993fc643d2e9d
poro625 <poro625@naver.com>
2022-10-26 20:34:26 +0900

    커뮤니티 모델  추가

--- community/models.py
+++ community/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return str(self.user)
