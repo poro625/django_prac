ab7ce607d8d7f3b4ae156791a8382356f40c0e8c
poro625 <poro625@naver.com>
2022-10-26 20:24:25 +0900

    회원정보 변경 완료

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,13 @@ app_name = 'users'
 
 urlpatterns = [
     
+    path('home/',views.home ,name='home'),
     path('signup/', views.signup ,name='signup'),
     path('login/',views.login ,name='login'),
-    path('home/',views.home ,name='home'),
+    path('logout/',views.logout ,name='logout'), # 로그아웃
+    path('delte/',views.delte ,name='delte'), #회원탈퇴
+    path('password/<int:id>/',views.password ,name='password'), # 비밀번호 변경 
+    path('detail/<int:id>/',views.detail ,name='detail'), # 회원정보 변경
+
     
 ]
